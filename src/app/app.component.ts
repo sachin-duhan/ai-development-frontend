8f8d423c864db72419eecd7fa6b9ae507c1998fa
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-07-11 03:05:11 +0530

    updating UI and looks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
+    }
+
     year = new Date().getFullYear();
-    
     navigation_list: Array<{ title: string, url: string }> = [
         { url: 'candidate', title: 'Candidate' },
         { url: 'evaluate', title: 'Interview' },
@@ -15,10 +26,10 @@ export class AppComponent {
         { url: 'interview', title: 'Interview' },
     ];
 
-    social_media_links:Array<{ url: string; path: string}> =[
+    social_media_links: Array<{ url: string; path: string }> = [
         { url: '#!', path: '/assets/social media/facebook.svg' },
-        { url: '#!', path: '/assets/social media/twitter.svg'},
-        { url: '#!', path: '/assets/social media/instagram.svg'},
+        { url: '#!', path: '/assets/social media/twitter.svg' },
+        { url: '#!', path: '/assets/social media/instagram.svg' },
         { url: '#!', path: '/assets/social media/youtube.svg' },
     ]
 }
